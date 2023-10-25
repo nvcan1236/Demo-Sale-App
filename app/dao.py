2a202cc3d452393406ce562816021e24824a74a8
ngcanh1236@gmail.com <ngcanh1236@gmail.com>
2023-10-25 16:57:09 +0700

    init project

---
+++ app/dao.py
@@ -0,0 +1,54 @@
+def get_categories():
+    return [
+        {id: 1,
+         'name': 'Mobile'
+         },
+        {id: 2,
+         'name': 'Tablet'
+         },
+        {id: 3,
+         'name': 'Computer'
+         },
+    ]
+
+
+def get_products():
+    products = [{
+        'id': 1,
+        'price': 32000000,
+        'name': 'iPhone 15',
+        'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+    },
+        {
+            'id': 2,
+            'name': 'iPhone 15',
+            'price': 32000000,
+            'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+        },
+        {
+            'id': 3,
+            'name': 'iPhone 15',
+            'price': 32000000,
+            'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+        },
+        {
+            'id': 4,
+            'name': 'iPhone 15',
+            'price': 32000000,
+            'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+        },
+        {
+            'id': 5,
+            'name': 'iPhone 15',
+            'price': 32000000,
+            'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+        },
+        {
+            'id': 6,
+            'name': 'iPhone 15',
+            'price': 32000000,
+            'image': 'https://mobileleb.com/cdn/shop/files/apple-mobile-phone-apple-iphone-15-pro-max-512gb-33291439046788_1024x1024.jpg?v=1694771068'
+        },
+    ]
+
+    return products
---
+++ app/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+import dao
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    cates = dao.get_categories()
+    products = dao.get_products()
+    return render_template('index.html', categories=cates, products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)
